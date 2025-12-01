2f63d6c089f7d4ff62234f8a9e9ca980e9f68df8
Sandip1000 <sanjelsandip10@gmail.com>
2025-12-01 14:03:34 +0545

    upto python input

--- python_inputs.py
+++ python_inputs.py
@@ -1,6 +1,4 @@
-#Python User Input
-
-
+#1. Python User Input
 """
 -> Python allows for user input using input() function. That means we are able to ask user for input.
 -> Python stops executing when it comes to the input() function, and continues when the user has given some input.
@@ -19,8 +17,56 @@ print(f"Hello {name}")
 print(type(name))   #input() always returns the string object
 
 
-#Taking Number as Input
+
+#2. Taking Multiple Inputs
+"""
+-> We can add as many inputs as we want, Python will stop executing at each of them, waiting for user input.
+"""
+
+name = input("Enter your name:")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+
+
+
+
+
+
+#3. Taking Number as Input
+"""
+-> The input from the user is treated as string. So, we have to convert the string to a number using int() or float() constructor.
+"""
 
 num=int(input("Enter any number: "))
 print(num)
 print(type(num))
+
+
+import math
+x = input("Enter a number:")
+
+#find the square root of the number:
+y = math.sqrt(float(x))
+
+print(f"The square root of {x} is {y}")
+
+
+
+
+#4. Validate Input
+"""
+-> It is good practice to validate any input from the user. It is useful to avoid the errors.
+"""
+y = True
+while y == True:
+  x = input("Enter a number:")
+  try:
+    x = float(x)
+    y = False
+  except:
+    print("Wrong input, please try again.")
+
+print("Thank you!")
